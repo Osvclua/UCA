8894ad38c585da731fc9ac8accb1ab826c4221f1
Osvclua <osvclua.varios@gmail.com>
2021-09-07 19:05:09 -0300

    clase

--- Python-C++/Cifras/cesar_ws.cc
+++ Python-C++/Cifras/cesar_ws.cc
@@ -12,22 +12,26 @@ string cesar (string st, int n){
 	string alfa("abcdefghijklmnopqrstuvwxyz ");
 	alfa+=alfa;
 	for (int i=0; i<st.length();i++)
-		r+=alfa[alfa.find_first_of(st[i])+n];
+		if (st[i]==' ')r=r+" ";
+		else
+		    r+=alfa[alfa.find_first_of(st[i])+n];
 	return r;
 }
 string rasec (string st, int n){
 	string r("");
-	string alfa("abcdefghijklmnopqrstuvwxyz ");
+	string alfa("abcdefghijklmnopqrstuvwxyz ");make virm cesar_ws
 	alfa+=alfa;
 	for (int i=0; i<st.length();i++)
-		r+=alfa[alfa.find_last_of(st[i])-n];
+		if (st[i]==' ') r=r+" ";
+		else
+			r+=alfa[alfa.find_last_of(st[i])-n];
 	return r;
 }
 
 
 int main(int argc, char* argv[]){
 	if (argc<3) {
-    cerr<<"Uso: "<<argv[0]<<" <nro clave> <archivo> "<<endl;
+    cerr<<"Uso: "<<argv[0]<<"<nro clave> <archivo> "<<endl;
     exit(1);
   }
 	string n_archi,n_sali,dato, datocif;
@@ -38,14 +42,13 @@ int main(int argc, char* argv[]){
 	n_sali=n_archi+".cesar";
 	ifstream archi(n_archi);
 	ofstream sali(n_sali);
-	archi.getline(&linea[0],60);  //archi>>dato no lee la linea 
+	archi.getline(&linea[0],60);
 	while (!archi.eof()){
 		dato=linea;
 		datocif=cesar(dato,clave);
 		sali<<datocif<<endl;
 		archi.getline(&linea[0],60);
 		}
-	cout<<"Cifrado el "<<n_archi<<" con clave "<<clave<<
-	" en "<<n_sali<<endl;
+	cout<<"Cifrado el "<<n_archi<<" en "<<n_sali<<endl;
 }
 
